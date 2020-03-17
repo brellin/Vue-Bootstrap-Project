632f32910e070e265153a7b2454b268cce4df6bd
brellin <cekakid@gmail.com>
2020-03-17 17:48:52 -0500

    create button
    allow 'content' prop
    minor styles on button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,1 +1,29 @@
-<template></template>
+<template>
+  <button>{{ content }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    content: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style lang='scss'>
+button {
+  box-shadow: none;
+  padding: 5px 10px;
+  border: 1px solid midnightblue;
+  border-radius: 2.5px;
+  cursor: pointer;
+  transition: 0.3s ease-in-out;
+
+  &:hover {
+    background: #c3c3ff;
+  }
+}
+</style>
--- src/components/News.vue
+++ src/components/News.vue
@@ -1,3 +1,13 @@
 <template>
-  <h2>News</h2>
+  <div class="news">
+    <h2>News</h2>
+    <Button content="View News" />
+  </div>
 </template>
+
+<script>
+import Button from "./Button";
+export default {
+  components: { Button }
+};
+</script>
