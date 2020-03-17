3c1b74dce1221df37fa900d68fc91d6f1c1f7d06
brellin <cekakid@gmail.com>
2020-03-17 16:19:30 -0500

    declare routes, components, and values to be displayed for each component

---
+++ src/components/Header.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Vue Routing</h1>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import Home from "./components/Home";
+import News from "./components/News";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/home', component: Home },
+  { path: '/news', component: News }
+];
+
+const router = new VueRouter({ routes });
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
